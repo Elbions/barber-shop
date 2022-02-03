4e33258c44e599b83d113b95a4c1d2b71b1faa7a
EljonasCavalcante <eljonascavalcante@hotmail.com>
2022-02-02 23:43:15 -0300

    feat:creating button

---
+++ barber-shop/src/components/button/button.tsx
@@ -0,0 +1,15 @@
+import { HTMLInputTypeAttribute, InputHTMLAttributes, useState  } from 'react';
+
+import styles from './button.module.scss';
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+    isPassword?: boolean;
+    placeholder?: string;
+}
+export function Button (props: Props) {
+    return (
+        <div>
+            <a>Entrar</a>
+        </div>
+    )
+}
--- barber-shop/src/pages/login/login.tsx
+++ barber-shop/src/pages/login/login.tsx
@@ -1,4 +1,5 @@
 import photoLogin from '../../assets/login.png';
+import { Button } from '../../components/button/button';
 import { Input } from '../../components/input/input';
 import styles from './login.module.scss';
 export function Login() {
@@ -8,6 +9,7 @@ export function Login() {
         <img src={photoLogin}></img>
             <Input placeholder="E-mail" /> 
             <Input placeholder='Senha'/> 
+            <Button/>
         </div>
     )
 }
