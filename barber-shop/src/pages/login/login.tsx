bb001ede9e1c79dab4ecc11596c83d524259c08d
Bianca Barbosa <yume_shiro@hotmail.com>
2022-02-04 01:59:39 -0300

    feat: creating style to login page

--- barber-shop/src/pages/login/login.tsx
+++ barber-shop/src/pages/login/login.tsx
@@ -3,18 +3,41 @@ import { Button } from '../../components/button/button';
 import { Input } from '../../components/input/input';
 import styles from './login.module.scss';
 import logo from '../../assets/icons/logo.png'
+import registerIcon from '../../assets/icons/imgregister.png'
 import emailIcon from '../../assets/icons/imgemail.png'
 import passwordIcon from '../../assets/icons/imgpassword.png'
 export function Login() {
     return (
-        <div>
-            <img src={logo}></img>
-            <img src={photoLogin}></img>
-            <p>Faça seu login</p>
-            <div>
-                <Input icon={emailIcon} placeholder="E-mail" /> 
-                <Input icon={passwordIcon} placeholder='Senha'/> 
-          </div>
+        <div className={styles.container}>
+
+            <div className={styles.loginPhotoContainer}>
+                <img src={photoLogin}></img>
+            </div>
+
+            <div className={styles.loginContainer}>
+
+                <div className={styles.loginLogoContainer}>
+                    <img src={logo}></img>
+                    
+                    <p>Faça seu login</p>
+                </div>
+
+                <div className={styles.loginBox}>
+                    <Input icon={emailIcon} placeholder="E-mail" /> 
+                    <Input icon={passwordIcon} placeholder='Senha'/>
+
+                    <Button /> 
+
+                    <div className={styles.loginFooter}>
+                        <p>Esqueci minha senha</p>
+                        
+                        <div className={styles.loginRegister}>
+                            <p>Criar conta</p>
+                            <img src={registerIcon}></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
