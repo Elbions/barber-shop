b0ea8bb12ee76309e69da1187a3e530ee8abbb56
EljonasCavalcante <eljonascavalcante@hotmail.com>
2022-02-06 00:06:07 -0300

    feat:update

---
+++ barber-shop/src/pages/register/register.tsx
@@ -0,0 +1,14 @@
+import styles from './register.module.scss';
+
+export function Register() {
+    return(
+        <div className={styles.container}>
+            
+
+             
+
+
+        </div>
+
+    );
+}
